56cf27ff705de181c01c6c39b6524e96d137365f
mevludeortac <ortacmevlude@gmail.com>
2021-07-26 22:44:37 +0300

    Ensuring FaceID  Control

--- FaceRecognition/ViewController.swift
+++ FaceRecognition/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtMessage: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func comeInClicked(_ sender: Any) {
+        let authContext = LAContext()
+        var error: NSError?
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "is it really you?") { (success, error) in
+                if success == true{
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toPutin", sender: nil)
+                    }
+                }
+                else{
+                    DispatchQueue.main.async {
+                        self.txtMessage.text = "error!"
+                    }
+                }
+            }
+        }
+        
+    }
 }
 
